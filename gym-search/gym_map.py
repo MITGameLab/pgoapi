aa424b10b2db15edb0d386d678cb055f8c82d305
Adrian Sy <adriansy@mit.edu>
2017-02-03 18:28:54 -0500

    Fixed import for gym map

--- gym-search/gym_map.py
+++ gym-search/gym_map.py
@@ -33,12 +33,15 @@ import pprint
 #import webbrowser
 import urllib
 
-from pgoapi import PGoApi
-from pgoapi import utilities as util
-
 from google.protobuf.internal import encoder
 from geopy.geocoders import GoogleV3
 from s2sphere import CellId, LatLng
+
+sys.path.append("../pgoapi-source/")
+
+from pgoapi import PGoApi
+from pgoapi import utilities as util
+
 
 log = logging.getLogger(__name__)
 
