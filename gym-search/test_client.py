e0d5cfe04f733be47b71c9026d8c7f99389b6eb1
Adrian Sy <adriansy@mit.edu>
2017-04-11 00:20:49 -0400

    Added test server and test client for simulation

---
+++ gym-search/test_client.py
@@ -0,0 +1,29 @@
+import urllib2
+import json
+import argon2
+import requests
+import led_grid
+import time
+
+NODE_URL = "http://127.0.0.1:8000/"
+
+def get_json(loc):
+    return json.loads(urllib2.urlopen(NODE_URL + "?loc="+str(loc)).read())
+
+def main():
+    while True:
+        forts = get_json("")
+        for fort in forts:
+            if 'owned_by_team' in fort:
+                
+                if fort['latitude'] == 42.359092 and fort['longitude'] == -71.093987:
+                    print "Alchemist Gym owned by Team " + str(fort['owned_by_team'])
+                    if 'is_in_battle' in fort:
+                        print "Alchemist Gym is in battle!"
+                        led_grid.show_team_battle(fort['owned_by_team'])
+                    else:
+                        led_grid.show_team_color(fort['owned_by_team'])
+        time.sleep(2)
+
+if __name__ == '__main__':
+    main()
---
+++ gym-search/test_server.py
@@ -0,0 +1,24 @@
+from flask import abort, Flask, jsonify, request
+
+app = Flask(__name__)
+forts = {"Alchemist" : {"id":1, "owned_by_team": 1,
+                        "latitude": 42.359092,
+                        "longitude": -71.093987,
+                        "is_in_battle": False},
+         "Stata Center": {"id":2, "owned_by_team":2,
+                        "latitude": 42.362172,
+                        "longitude": -71.090021,
+                        "is_in_battle": False}}
+@app.route("/")
+def index():
+    loc = request.args.get("loc","")
+    results = []
+    if str(loc) in forts:
+        results.append(forts[str(loc)])
+    elif len(str(loc))==0:
+        results = forts.values()
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run(port=8000)
+    
